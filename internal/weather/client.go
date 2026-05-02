797df72d2564c1ff90c1dc62a1c7239962947d21
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-05-02 21:11:50 +0545

    added client.go file

---
+++ internal/weather/client.go
@@ -0,0 +1,49 @@
+package weather
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Client struct {
+	baseURL    string
+	httpClient *http.Client
+}
+
+func NewClient(baseURL string) *Client {
+	return &Client{
+		baseURL: baseURL,
+		httpClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func (c *Client) GetWeather(coords Coordinates) (*WeatherResponse, error) {
+	params := url.Values{}
+	params.Set("latitude", fmt.Sprintf("%f", coords.Latitude))
+	params.Set("longitude", fmt.Sprintf("%f", coords.Longitude))
+	params.Set("current_weather", "true")
+
+	fullURL := fmt.Sprintf("%s?%s", c.baseURL, params.Encode())
+
+	resp, err := c.httpClient.Get(fullURL)
+	if err != nil {
+		return nil, fmt.Errorf("request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("upstream returned %d", resp.StatusCode)
+	}
+
+	var result WeatherResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("decode failed: %w", err)
+	}
+
+	return &result, nil
+}
