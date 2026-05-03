aeed42e1d3a32363c591e6a3929de0e92d051a73
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-05-03 14:23:37 +0545

    yeah added some other functionalitues ( updated periodially or sum )

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,24 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/Saugat-Tamang17/weather-wrapper/internal/weather"
+)
+
+type WeatherHandler struct {
+	client *weather.Client
+}
+
+func New(client *weather.Client) *WeatherHandler {
+	return &WeatherHandler{client: client}
+}
+
+func (h *WeatherHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	latsstr := r.URL.Query().Get("lat")
+	longstr := r.URL.Query().Get("lng")
+
+	if latsstr == "" || longstr == "" {
+		http.Error(w, "Latitude and Longitude  ")
+	}
+}
