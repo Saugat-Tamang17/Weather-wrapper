da470e072353d6a389ca15e2fd19a1c2d838d2e6
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-05-03 15:22:47 +0545

    fixed the structure to match the current json tag data

--- internal/weather/models.go
+++ internal/weather/models.go
@@ -1,17 +1,21 @@
 package weather
 
+type Coordinates struct {
+	Latitude  float64
+	Longitude float64
+}
+
 type CurrentWeather struct {
 	Temperature float64 `json:"temperature_2m"`
 	Windspeed   float64 `json:"windspeed_10m"`
 	WeatherCode int     `json:"weathercode"`
 	IsDay       int     `json:"is_day"`
-	Humidity    float64 `json:"relative_humidity_2m"`
+	Humidity    int     `json:"relative_humidity_2m"`
 }
 
 type WeatherResponse struct {
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	Timezone  string  `json:"timezone"`
-
-	Current CurrentWeather `json:"current"`
+	Latitude  float64        `json:"latitude"`
+	Longitude float64        `json:"longitude"`
+	Timezone  string         `json:"timezone"`
+	Current   CurrentWeather `json:"current"`
 }
