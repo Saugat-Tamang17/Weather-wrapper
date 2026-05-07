ee8d205cb43941e7d3a9f84500281fc3fac4fed8
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-05-07 17:17:21 +0545

    added the testing case  1 , missing params

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -1,1 +1,33 @@
 package handler
+
+import (
+	"github.com/Saugat-Tamang17/weather-wrapper/internal/weather"
+)
+
+type fakeClient struct {
+	response *weather.WeatherResponse
+	err      error
+}
+
+func (f *fakeClient) GetWeather(coords weather.Coordinates) (*weather.WeatherResponse, error) {
+	return f.response, f.err
+}
+
+func TestWeatherHandler(t *testing) {
+	tests := []struct {
+		name       string
+		url        string
+		fakeResp   *weather.WeatherResponse
+		fakeErr    error
+		wantStatus int
+	}{
+		//case 1 //
+		name: "missing params",
+		url: "/weather",
+		wantStatus: 400,
+
+	},
+	{
+		
+	}
+}
