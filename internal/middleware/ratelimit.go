0a3ee8a035266787445c18d06d6bf01f3553de4d
Saugat-Tamang17 <saugattamang693@gmail.com>
2026-05-09 22:06:46 +0545

    added the struct ( base ) for the rate limiter

---
+++ internal/middleware/ratelimit.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"sync"
+
+	"golang.org/x/time/rate"
+)
+
+type RateLimiter struct {
+	mu       sync.Mutex
+	limiters map[string]*rate.Limiter
+	rate     rate.Limit
+	burst    int
+}
